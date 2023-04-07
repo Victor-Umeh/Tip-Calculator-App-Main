91fc9b23b93827f28c8391eda09c1de82229f121
vk196 <victorumeh196@gmail.com>
2023-04-07 09:58:31 -0700

    Tip percentage buttons made active

--- tip-calculator-app-main/index.js
+++ tip-calculator-app-main/index.js
@@ -1,5 +1,5 @@
 "use strict";
-const billValue = document.getElementById("bill");
+const billValue = document.querySelector("#bill");
 const numOfPeople = document.getElementById("people");
 const tipPercentages = document.querySelectorAll(".tips");
 const customTipPercentages = document.querySelector(".custom");
@@ -8,23 +8,17 @@ const tipAmount = document.querySelector(".per__person");
 const totalPerson = document.querySelector(".total__person");
 
 let tip = 0;
+console.log(billValue.textContent);
 
 tipPercentages.forEach((e, i) => {
   e.addEventListener("click", tipsFunc);
 });
 
 function tipsFunc(e) {
+  document.querySelector(".active")?.classList.remove("active"); //Removes active class
   const element = e.target;
   tip = Number(element.textContent.replace("%", ""));
-  console.log(tip);
+  element.classList.add("active");
+  //   console.log(tip);
 }
-
-// if (element.classList.contains("active")) {
-//     element.classList.remove("active");
-//     if (!element.classList.contains("active")) {
-//       !element && element.classList.remove("active");
-//     }
-//   } else {
-//     element.classList.add("active");
-//   }
-//   console.log(!element);
+// if(billValue.textContent === )
