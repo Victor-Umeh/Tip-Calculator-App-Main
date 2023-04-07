c593acab9384fa9c05f8c66f6ca32f4e88934745
vk196 <victorumeh196@gmail.com>
2023-04-07 05:43:38 -0700

    HTML node selections in JS

--- tip-calculator-app-main/index.js
+++ tip-calculator-app-main/index.js
@@ -1,1 +1,19 @@
 "use strict";
+const billValue = document.getElementById("bill");
+const numOfPeople = document.getElementById("people");
+const tipPercentages = document.querySelectorAll(".tips");
+const customTipPercentages = document.querySelector(".custom");
+const resetBtn = document.querySelector(".reset");
+const tipAmount = document.querySelector(".per__person");
+const totalPerson = document.querySelector(".total__person");
+
+tipPercentages.forEach((e) => {
+  e.addEventListener("click", tipsFunc);
+});
+
+let tip;
+function tipsFunc(e) {
+  tip = e.target.textContent;
+  e.target.classList.add("active");
+  console.log(tip);
+}
